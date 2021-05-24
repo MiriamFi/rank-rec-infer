a7186c7e7930be951f9b680cf25bb58b6cc5a375
MiriamFi <mfinjord@gmail.com>
2021-05-24 22:24:49 +0200

    Logistic regression for age, occ and loc

--- rfm.py
+++ rfm.py
@@ -8,9 +8,17 @@ from rankfm.rankfm import RankFM
 from rankfm.evaluation import precision, recall
 
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+
+from uszipcode import SearchEngine
 
 # Constants
 K = 10
+
+STATE = "state"
+CITY = "major_city"
+
+LOC_TYPE = STATE
 
 INCLUDE_FEATURES = {
         "gender" : True,
@@ -19,6 +27,14 @@ INCLUDE_FEATURES = {
         "state" : False,
         "city" : False
         }
+
+AGE_GROUPS = {
+    0 : [0,34],
+    1 : [35,45],
+    2 : [46,99]
+}
+
+
 
 
 # Load interaction data
@@ -55,9 +71,9 @@ def load_user_data(filename="u.user", path="ml-100k/"):
             user_id, age, gender, occu, zipcode = line.split('|')
             user_info[int(user_id)-1] = {
                 'age': int(age),
-                'gender': 1 if gender== "M" else 0,
+                'gender': gender,
                 'occupation': occu,
-                'zipcode': str(zipcode)
+                'location': str(zipcode).strip()
             }
         print('User Info Loaded!')
     return user_info
@@ -135,20 +151,51 @@ def write_recommendations_to_csv(recommendations, scores):
                 csv_writer.writerow([ usr, recommendations[rnk][usr], rnk+1, confidence_scores[rnk][usr] ] )
             ind += 1
 
-def prepare_attributes_for_classifier(user_info, attr="gender"):
+def prepare_attributes_for_classifier(user_info, attr_type="gender"):
+    attr_classes = {}
     attributes = []
+
+    def is_in_age_group(age, age_cat):
+        return True if age >= AGE_GROUPS[age_cat][0] and age <= AGE_GROUPS[age_cat][1] else False
+    
+    # Map zip code to state/city
+    def map_location(zipcode, loc_type):
+        search = SearchEngine(simple_zipcode=True)
+        zip_code = search.by_zipcode(zipcode)
+        zip_code = zip_code.to_dict()
+        return zip_code[loc_type]
+
     for usr_id in user_info.keys():
-        attributes.append(user_info[usr_id][attr])
+        attr_value = user_info[usr_id][attr_type]
+
+        if attr_type == "age":
+            for age_cat in AGE_GROUPS.keys():
+                if is_in_age_group(attr_value, age_cat):
+                    new_attr_value = age_cat
+        else:
+            if attr_type == "location":
+                attr_value = map_location(attr_value, LOC_TYPE)
+            # Create dict of attribute labels
+            if attr_value not in attr_classes.keys():
+                attr_classes[attr_value] = len(attr_classes)
+            new_attr_value = attr_classes[attr_value]
+
+        # Create array of attribute representations
+        attributes.append(new_attr_value)
     return attributes
 
 # Try with gender first, so age, then occupation
 # X is recommendaitons for each user (943, 10)
 # z is true genders shape(943,1)
-def apply_logistic_regression(X, y):
-    clf = LogisticRegression(random_state=0, max_iter=100).fit(X, y)
+# This does not have the correct splits, right now it is trained and tested on the same set
+def apply_logistic_regression(X, y, max_iter=100):
+    clf = LogisticRegression(random_state=0, max_iter=max_iter).fit(X, y)
     print(clf.predict(X), "\n\n")
     print(clf.predict_proba(X), "\n\n")
     print(clf.score(X, y))
+
+def apply_svm(X,y):
+    clf = svm.SVC().fit(X,y)
 
 
 
@@ -201,13 +248,27 @@ def main():
     # Write recommendation results to file
     write_recommendations_to_csv(test_recommendations, test_scores)
 
-    # Prepare attributes for classification
-    gender_attributes = prepare_attributes_for_classifier(user_info, attr="gender")
-    print("Gender attributes len: ", len(gender_attributes))
-    print(gender_attributes)
+    # Prepare  attributes for classification
+    attributes = {}
+    #attributes["gender"] = prepare_attributes_for_classifier(user_info, attr_type="gender")
+    #attributes["age"] = prepare_attributes_for_classifier(user_info, attr_type="age")
+    #attributes["occupation"] = prepare_attributes_for_classifier(user_info, attr_type="occupation")
+    attributes["location"] = prepare_attributes_for_classifier(user_info, attr_type="location")
+    #print("Gender attributes len: ", len(attributes["gender"]))
+    #print("Age attributes len: ", len(attributes["age"]))
+    #print("Occupation attributes len: ", len(attributes["occupation"]))
+    print("Location attributes len: ", len(attributes["location"]))
+    #print("gender attributes: ", attributes["gender"])
+    #print("age attributes: ", attributes["age"])
+    #print("occupation attributes: ", attributes["occupation"])
+    print("location attributes: ", attributes["location"])
 
-    # Classify
-    apply_logistic_regression(test_recommendations, gender_attributes)
+    # Classify gender
+    #apply_logistic_regression(test_recommendations, attributes["gender"])
+    #apply_logistic_regression(test_recommendations, attributes["age"])
+    #apply_logistic_regression(test_recommendations, attributes["occupation"], max_iter=120)
+    apply_logistic_regression(test_recommendations, attributes["location"], max_iter=120)
+
 
 
 
