cff7d43fb3606171e0fc7b5a42180f8733b2fd1c
MiriamFi <mfinjord@gmail.com>
2021-06-03 15:53:56 +0200

    Added customized splits to draft

--- spt.py
+++ spt.py
@@ -23,18 +23,36 @@ def load_data(filename="u.data", path="ml-100k/"):
     data = pd.DataFrame(data=data)
     return data
 
-def prepare_splits(data):
+def prepare_splits(data, train_size=0.9, test_size=0.1):
     ranks = data.groupby('user_id')['ts'].rank(method='first')
     counts = data['user_id'].map(data.groupby('user_id')['ts'].apply(len))
-    thresholds = (ranks/counts) > 0.8
-    thresholds = pd.DataFrame(thresholds, columns=["thrs"])
-    print(thresholds)
-    data = data.join(thresholds)
-    print(data)
-    X_train = data[data['thrs'] == False]
-    X_test = data[data['thrs'] == True]
+    thrs_train = (ranks/counts) <= train_size
+    thres_train = pd.DataFrame(thrs_train, columns=["thrs_train"])
+    data = data.join(thres_train)
+    X_train = data[data['thrs_train'] == True]
     print("X_train: ", X_train)
-    print("X_test: ", X_test)
+    X_train = X_train.drop(columns='thrs_train', axis=1)
+    print("X_train: ", X_train)
+    if train_size + test_size == 1.0:
+        X_test = data[data['thrs_train'] == False]
+        print("X_test: ", X_test)
+        X_test = X_test.drop(columns='thrs_train', axis=1)
+        print("X_test: ", X_test)
+    else:
+        thrs_test = (ranks/counts) <= (train_size + test_size)
+        thres_test = pd.DataFrame(thrs_test, columns=["thrs_test"])
+        data = data.join(thres_test)
+        data["thrs"] = data["thrs_test"] > data["thrs_train"]
+        X_test = data[data['thrs'] == True]
+        print("X_test: ", X_test)
+        X_test = X_test.drop(columns=['thrs', 'thrs_train', 'thrs_test'], axis=1)
+        print("X_test: ", X_test)
+    
+    
+    
+    
+
+
     return (X_train, X_test)
 
 
